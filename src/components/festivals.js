6a90c6033228f77c84869c405abb02d7fb143313
mohit <mvk1407@gmail.com>
2021-10-22 16:42:33 +0530

    first push

---
+++ src/components/festivals.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+import '../index.css';
+
+
+function Festivals(){
+  return (
+    <>
+   
+
+      <div className="content">
+        <div className="hero-1">
+          <h3> Culture of India </h3>
+          <p> The culture of India refers collectively to the thousands of distinct and unique cultures of all religions and communities present in India. India's languages, religions, dance, music, architecture, food and customs differ from place to place within the country. Indian culture, often labeled as an amalgamation of several cultures, spans across the Indian subcontinent and has been influenced by a history that is several millennia old. Many elements of India's diverse cultures, such as Indian religions, philosophy, cuisine, languages, dance, music and movies have a profound impact across the Indosphere, Greater India and the world.
+            </p>
+            <img src="https://image.freepik.com/free-vector/indian-set-temples-architecture_25030-39369.jpg" />
+          </div>
+          </div>
+    </>
+  )
+}
+
+export default Festivals;
