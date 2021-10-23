2b29eb85a03fe7ac74b58bae81fc6412e429c20b
mohit <mvk1407@gmail.com>
2021-10-23 14:32:14 +0530

    completed dance.js

--- src/components/clothing.js
+++ src/components/clothing.js
@@ -1,91 +1,106 @@
-import React from 'react'
+import React from "react";
 import "./clothing.css";
 
 function Clothing() {
-    return (
-        <div className="container">
+  return (
+    <div className="content">
+      <div className="dance-1">
+        <h3> Indian Clothing & Attires </h3>
+        <p>
+          {" "}
+          Clothing in India is dependent upon the different ethnicity,
+          geography, climate, and cultural traditions of the people of each
+          region of India. Historically, male and female clothing has evolved
+          from simple garments like kaupina, langota, dhoti, lungi, sari,
+          gamcha, and loincloths to cover the body into elaborate costumes not
+          only used in daily wear, but also on festive occasions, as well as
+          rituals and dance performances. In urban areas, western clothing is
+          common and uniformly worn by people of all social levels. India also
+          has a great diversity in terms of weaves, fibers, colours, and
+          material of clothing. Sometimes, color codes are followed in clothing
+          based on the religion and ritual concerned. The clothing in India also
+          encompasses the wide variety of Indian embroidery, prints, handwork,
+          embellishment, styles of wearing clothes. A wide mix of Indian
+          traditional clothing and western styles can be seen in India.
+        </p>
+      </div>
 
-            <div className="card">
-                <div className="bg-image">
-                    <img src="../images/kashmiri-ladies.jpg" alt="bla" />
-                </div>
-                <div className="info">
-                    <h3>Kashmir Dress</h3>
-                </div>
-            </div>
-            <div className="card">
-                <div className="bg-image">
-                    <img src="../images/Punjabcolors.jpg" alt="bla" />
-                </div>
-                <div className="info">
-                    <h3>Punjabi Dress</h3>
-                </div>
-            </div>
-            <div className="card">
-                <div className="bg-image">
-                    <img src="../images/nepali.jpg" alt="bla" />
-                </div>
-                <div className="info">
-                    <h3>Nepali Dress</h3>
-                </div>
-            </div>
-            <div className="card">
-                <div className="bg-image">
-                    <img src="../images/muslim.jpg" alt="bla" />
-                </div>
-                <div className="info">
-                    <h3>Muslim Dress</h3>
-                </div>
-            </div>
-            <div className="card">
-                <div className="bg-image">
-                    <img src="../images/men.jpg" alt="bla" />
-                </div>
-                <div className="info">
-                    <h3>Men Wear</h3>
-                </div>
-            </div>
-            <div className="card">
-                <div className="bg-image">
-                    <img src="../images/tibat.jpg" alt="bla" />
-                </div>
-                <div className="info">
-                    <h3>Tibetian Wear</h3>
-                </div>
-            </div>
-            <div className="card">
-                <div className="bg-image">
-                    <img src="../images/lahnga.jpg" alt="bla" />
-                </div>
-                <div className="info">
-                    <h3>Lehenga</h3>
-                </div>
-            </div>
-            <div className="card">
-                <div className="bg-image">
-                    <img src="../images/saree.jpg" alt="bla" />
-                </div>
-                <div className="info">
-                    <h3>Saree</h3>
-                </div>
-            </div>
-            <div className="card">
-                <div className="bg-image">
-                    <img src="../images/indian.jpg" alt="bla" />
-                </div>
-                <div className="info">
-                    <h3>South Wear</h3>
-                </div>
-            </div>
+      <div className="container">
+        <div className="card">
+          <div className="bg-image">
+            <img src="../images/kashmiri-ladies.jpg" alt="bla" />
+          </div>
+          <div className="info">
+            <h3>Kashmir Dress</h3>
+          </div>
         </div>
-
-
-
-
-
-
-    );
+        <div className="card">
+          <div className="bg-image">
+            <img src="../images/Punjabcolors.jpg" alt="bla" />
+          </div>
+          <div className="info">
+            <h3>Punjabi Dress</h3>
+          </div>
+        </div>
+        <div className="card">
+          <div className="bg-image">
+            <img src="../images/nepali.jpg" alt="bla" />
+          </div>
+          <div className="info">
+            <h3>Nepali Dress</h3>
+          </div>
+        </div>
+        <div className="card">
+          <div className="bg-image">
+            <img src="../images/muslim.jpg" alt="bla" />
+          </div>
+          <div className="info">
+            <h3>Muslim Dress</h3>
+          </div>
+        </div>
+        <div className="card">
+          <div className="bg-image">
+            <img src="../images/men.jpg" alt="bla" />
+          </div>
+          <div className="info">
+            <h3>Men Wear</h3>
+          </div>
+        </div>
+        <div className="card">
+          <div className="bg-image">
+            <img src="../images/tibat.jpg" alt="bla" />
+          </div>
+          <div className="info">
+            <h3>Tibetian Wear</h3>
+          </div>
+        </div>
+        <div className="card">
+          <div className="bg-image">
+            <img src="../images/lahnga.jpg" alt="bla" />
+          </div>
+          <div className="info">
+            <h3>Lehenga</h3>
+          </div>
+        </div>
+        <div className="card">
+          <div className="bg-image">
+            <img src="../images/saree.jpg" alt="bla" />
+          </div>
+          <div className="info">
+            <h3>Saree</h3>
+          </div>
+        </div>
+        <div className="card">
+          <div className="bg-image">
+            <img src="../images/indian.jpg" alt="bla" />
+          </div>
+          <div className="info">
+            <h3>South Wear</h3>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-
-export default Clothing
+export default Clothing;
--- src/components/dance.js
+++ src/components/dance.js
@@ -33,20 +33,48 @@ function Dance() {
               <h4> Bharatnatyam, Tamilnadu</h4>
             </div>
             <div className="card_category_3">
-              <img src="https://www.holidify.com/images/cmsuploads/compressed/3531225584_d8f9ce5fd5_b_20170926204243.jpg" />
-              <h4> Punjab: Makki Di Roti & Sarson Da Saag</h4>
+              <img src="https://www.holidify.com/images/cmsuploads/compressed/33405125866_02c58f6e8a_b_20170926204513.jpg" />
+              <h4> Kathak, Uttar Pradesh</h4>
             </div>
             <div className="card_category_3">
-              <img src="https://www.holidify.com/images/cmsuploads/compressed/3531225584_d8f9ce5fd5_b_20170926204243.jpg" />
-              <h4> Punjab: Makki Di Roti & Sarson Da Saag</h4>
+              <img src="https://www.holidify.com/images/cmsuploads/compressed/449px-Kathakalidancer_20170926204612.jpg" />
+              <h4> Kathakali, Kerela</h4>
             </div>
             <div className="card_category_3">
-              <img src="https://www.holidify.com/images/cmsuploads/compressed/3531225584_d8f9ce5fd5_b_20170926204243.jpg" />
-              <h4> Punjab: Makki Di Roti & Sarson Da Saag</h4>
+              <img src="https://www.holidify.com/images/cmsuploads/compressed/4048039772_4a3629ce62_o_20170927125955.jpg" />
+              <h4>  Kuchipudi, Andhra Pradesh</h4>
             </div>
             <div className="card_category_3">
-              <img src="https://www.holidify.com/images/cmsuploads/compressed/3531225584_d8f9ce5fd5_b_20170926204243.jpg" />
-              <h4> Punjab: Makki Di Roti & Sarson Da Saag</h4>
+              <img src="https://www.holidify.com/images/cmsuploads/compressed/Garba_20180310125915.jpg" />
+              <h4> Garba, Gujarat</h4>
+            </div>
+            <div className="card_category_3">
+              <img src="https://www.holidify.com/images/cmsuploads/compressed/4900987611_68e8e1eed4_b_20170927130314.jpg" />
+              <h4> Ghoomar, Rajasthan</h4>
+            </div>
+            <div className="card_category_3">
+              <img src="https://www.holidify.com/images/cmsuploads/compressed/8451130049_7b1c16fd8c_b_20170927130153.jpg" />
+              <h4>  Bhangra/Gidda, Punjab</h4>
+            </div>
+            <div className="card_category_3">
+              <img src="https://www.holidify.com/images/cmsuploads/compressed/Lavani_20180314221552.JPG" />
+              <h4>  Lavani, Maharashtra</h4>
+            </div>
+            <div className="card_category_3">
+              <img src="https://www.holidify.com/images/cmsuploads/compressed/Bihu_of_Assam_20180314220215.jpg" />
+              <h4>  Bihu, Assam </h4>
+            </div>
+            <div className="card_category_3">
+              <img src="            https://www.holidify.com/images/cmsuploads/compressed/Rauff_20180310130603.jpg" />
+              <h4>  Rouf, Kashmir </h4>
+            </div>
+            <div className="card_category_3">
+              <img src="https://www.holidify.com/images/cmsuploads/compressed/5906208752_6d6dd71bb3_b_20170927130058.jpg" />
+              <h4>  Odissi, Odisha </h4>
+            </div>
+            <div className="card_category_3">
+              <img src="https://www.holidify.com/images/cmsuploads/compressed/486352442_28c34cbf1b_b_20170926204726.jpg" />
+              <h4>  Manipuri, Manipur</h4>
             </div>
             </div>
             </div>
